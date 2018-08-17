8d5c2f8e8d9d146f8eae8e4a8c5974d22046a4f0
MingMingZe <1137535813@qq.com>
2018-08-17 21:50:51 +0800

    init 一下

---
+++ lovezq.py
@@ -0,0 +1,25 @@
+import time
+from functools import lru_cache
+
+
+def clock(func):
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        args_str = ''.join(repr(arg) for arg in args)
+        print("[%0.8fs]%s(%s) -> %r" % (elapsed, name, args_str, result))
+        return result
+
+    return clocked
+
+
+@lru_cache()
+@clock
+def fibonacci(n):
+    return fibonacci(n - 2) + fibonacci(n - 1) if n >= 2 else n
+
+
+if __name__ == '__main__':
+    print(fibonacci(10))
