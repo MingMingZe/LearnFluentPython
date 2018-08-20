59ee25436e4ecc155d0bca30a7ade33907f05c7c
MingMingZe <1137535813@qq.com>
2018-08-20 20:16:51 +0800

    主要讲的是类变量和实例变量之间的关系。

---
+++ class_instance.py
@@ -0,0 +1,14 @@
+class MingMing:
+    name = 'human'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+ming = MingMing("Ming", 20)
+print("instance.name:", ming.name, " class.name:", MingMing.name)
+MingMing.name = 'Girl'
+ming.name = 'Boy'
+print("class.name:", MingMing.name)
+print("instance.name:", ming.name)
