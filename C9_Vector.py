96de5eed37b4b19136457e2a5e6ce87e7cc6a9f1
MingMingZe <1137535813@qq.com>
2018-08-30 21:23:23 +0800

    向量类，主要是实现各种内置方法，以及学会用格式规范语言去定义输出

---
+++ C9_Vector.py
@@ -0,0 +1,77 @@
+from array import array
+import math
+
+
+class Vector2d:
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def angle(self):
+        return math.atan2(self.x, self.y)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return "{}({!r},{!r})".format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+
+if __name__ == '__main__':
+    print(format(Vector2d(1, 1), 'p'))
+    print(format(Vector2d(1, 1), '.3ep'))
+    print(format(Vector2d(1, 1), '0.5fp'))
+    v1 = Vector2d(3, 4)
+    v2 = Vector2d(3.1, 4.1)
+    octets = bytes(v1)
+    print(octets)
+    print(hash(v1), hash(v2))
+    v1_clone = Vector2d.frombytes(bytes(v1))
+    print(v1_clone == v1)
+    print(len(set([v1, v2])))
