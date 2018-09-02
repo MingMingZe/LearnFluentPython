65082df637ca902d1dde0753d9cbec27290dc483
MingMingZe <1137535813@qq.com>
2018-09-02 15:07:26 +0800

    添加了打印的结果

--- MyYield.py
+++ MyYield.py
@@ -18,3 +18,24 @@ for i in range(9):
 
 for item in gen_fibonacci2(9):
     print("gen_fibonacci2", item)
+
+
+# 打印结果：
+# normal： 0
+# normal： 1
+# normal： 1
+# normal： 2
+# normal： 3
+# normal： 5
+# normal： 8
+# normal： 13
+# normal： 21
+# gen_fibonacci2 0
+# gen_fibonacci2 1
+# gen_fibonacci2 1
+# gen_fibonacci2 2
+# gen_fibonacci2 3
+# gen_fibonacci2 5
+# gen_fibonacci2 8
+# gen_fibonacci2 13
+# gen_fibonacci2 21
